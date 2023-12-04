df329d4a56a018e1f1860631d9b26342c7413064
superH-text <108380766+superH-text@users.noreply.github.com>
2023-12-04 10:51:21 +0800

    第一次提交：设置了启动应用动画

---
+++ app/src/main/java/com/xxxy/lihang/hismarthome/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.xxxy.lihang.hismarthome.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        //getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_splash);
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);//使程序休眠五秒
+                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+    }
+}
