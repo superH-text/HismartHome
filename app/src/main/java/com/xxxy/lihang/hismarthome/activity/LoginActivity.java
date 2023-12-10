990e93238c35be7503a4cbb1647f01a17489f42c
superH-text <108380766+superH-text@users.noreply.github.com>
2023-12-10 17:33:23 +0800

    第三次提交： 完成：登录页面，将登录的页面全部展示出来， 未完成：1.登录功能；2.注册账号的跳转；3.页面的返回上一级；4.手机号登录；5.忘记密码的操作，6.其他登录方式（调用微信接口进行访问登录） 下一步的计划：实现返回上一级操作，以及登录注册的功能实现（利用后端servlet和数据库的使用）

---
+++ app/src/main/java/com/xxxy/lihang/hismarthome/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.xxxy.lihang.hismarthome.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
